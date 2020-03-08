aa22180753e8cc90f3d5f81922c5b9c5a1285047
max-student <max-student@maxtrain.com>
2020-03-08 15:27:19 -0400

    Add project files.

---
+++ CSharpAnd EntityFramework/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CSharpAnd_EntityFramework {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
